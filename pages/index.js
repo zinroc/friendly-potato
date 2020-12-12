f199e72f91f89d0f917e665d3a18f37ed234c1d6
Sergei <sergei.vilbik@saturdayinsurance.com>
2020-12-12 17:49:25 -0500

    initialize emotion

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,29 @@
 import Head from "next/head";
-import styles from "../styles/Home.module.css";
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 
+import styled from "@emotion/styled";
+
 import { SET_GREETING } from "../redux/modules/main";
+
+const Header = styled.div`
+  background-color: black;
+  text-align: center;
+  width: 100%;
+  color: white;
+  padding: 10px;
+  font-weight: 500;
+  cursor: default;
+  position: fixed;
+  justify-content: space-around;
+  display: flex;
+  padding: 10px;
+  font-size: 25px;
+`;
+
+const Spacer = styled.div`
+  height: ${({ height }) => (height ? height : "0px")};
+`;
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -15,11 +35,14 @@ export default function Home() {
   const greeting = useSelector((state) => state.main.greeting);
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Sergei - Github Repo Lister</title>
+        <link rel="icon" href="/favicon.png" />
       </Head>
+      <Header>Github Repo Lister</Header>
+      <Spacer height="125px" />
+
       {greeting}
     </div>
   );
