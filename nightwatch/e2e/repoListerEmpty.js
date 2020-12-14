4aeed90feff3c9e114890509098f44853b08dcef
Sergei <sergei.vilbik@saturdayinsurance.com>
2020-12-14 10:28:03 -0500

    empty search - nightwatch

--- nightwatch/e2e/repoListerEmpty.js
+++ nightwatch/e2e/repoListerEmpty.js
@@ -1,16 +1,16 @@
 const SEARCH_INPUT = '//input[@name="searchInput"]';
 const SUBMIT_SEARCH = '//button[@name="submitSearch"]';
-const REPO = "//div[text()='zinroc/friendly-potato']";
+const LISTING_FOR_USER = "//h2[@name='listingForUser']";
 
 module.exports = {
-  "Repo Lister - User": (browser) => {
+  "Repo Lister - Empty": (browser) => {
     browser
       .url("http://localhost:3000/")
       .waitForElementVisible("body")
       .useXpath()
-      .setValue(SEARCH_INPUT, "zinroc")
+      .setValue(SEARCH_INPUT, "")
       .click(SUBMIT_SEARCH)
-      .waitForElementVisible(REPO)
+      .waitForElementNotPresent(LISTING_FOR_USER)
       .end();
   },
 };
